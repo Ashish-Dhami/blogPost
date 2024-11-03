2ec100191696321e70f4b6241d46da0472fa245b
Ashish Dhami <ashish.dhami.career@gmail.com>
2024-11-04 03:00:16 +0530

    changed the repo url

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -26,7 +26,7 @@ function Home() {
                         <div
                             className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
                             <a
-                                href="https://github.com/Ashish-Dhami"
+                                href="https://github.com/Ashish-Dhami/blogPost"
                                 className="px-8 py-3 text-lg font-semibold rounded bg-[#FC4370]"
                             >
                                 Code available here
